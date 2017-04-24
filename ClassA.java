cdd33af58a1e961462768ad84368ede83d024e8c
SmileJM <lovelyeu@hanmail.net>
2017-04-24 12:20:51 +0900

    수정함

--- ClassA.java
+++ ClassA.java
@@ -2,5 +2,7 @@ public calss ClassA {
   public static void main(String[] arges) {
     System.out.println("uesr1이 작성");
     System.out.println("uesr2가 작성");
+    System.out.println("uesr2가 작성");
+    System.out.println("uesr2가 작성");
   }
 }
