8ef1f702f48d5217085b46ff692986bf9d37ec3f
SmileJM <lovelyeu@hanmail.net>
2017-04-25 15:16:05 +0900

    Spring 처음

---
+++ WebApplication/src/java/com/mycompany/myapp/HomeController.java
@@ -0,0 +1,19 @@
+package com.mycompany.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("home");
+        return "home";
+    }
+    @RequestMapping("/html")
+    public String html() {
+        return "html";
+        // 리턴 값이 .jsp 파일과 관계가 있음        
+    }
+}
