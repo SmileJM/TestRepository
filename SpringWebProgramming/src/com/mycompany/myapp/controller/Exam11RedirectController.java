88fa2ab97dcac7dbbcdac4e27ee21281874546d1
SmileJM <lovelyeu@hanmail.net>
2017-05-24 14:36:38 +0900

    리다이렉트

---
+++ SpringWebProgramming/src/com/mycompany/myapp/controller/Exam11RedirectController.java
@@ -0,0 +1,25 @@
+package com.mycompany.myapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Exam11RedirectController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Exam11RedirectController.class);
+	
+	@RequestMapping("/redirect/write")
+	public String write() {
+		LOGGER.info("게시물 저장");
+		// 리다이렉트 해라 /redirect/list 이쪽으로
+		return "redirect:/redirect/list";
+	}
+
+	@RequestMapping("/redirect/list")
+	public String list() {
+		LOGGER.info("게시물 보기");
+		return "redirect/list";
+	}
+	
+}
