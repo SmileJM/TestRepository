fe330510a0d8c78dd4d930867c62809a18879942
SmileJM <lovelyeu@hanmail.net>
2017-08-17 10:08:48 +0900

    파일 삭제
    불필요한 것들

--- MpuWebProjectTest/src/main/java/com/mycompany/myapp/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.myapp.service;
-
-import com.mycompany.myapp.dto.Member;
-
-public interface MemberService {
-	public Member getMember(String memail);
-	public void memberJoin(Member member);
-}
